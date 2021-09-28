068d1faf9f753f715c9a168bf0cfed76b25951e7
itsnotrin <ryanwfun@gmail.com>
2021-09-28 09:57:17 +0100

    Login system works

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import requests
 # Homework URL: https://www.classcharts.com/apipublic/homework
 # Login URL: https://www.classcharts.com/apiv2student/login
 
+# Login System:
 login_url = 'https://www.classcharts.com/apiv2student/login'
 headers = {'User-Agent': 'Mozilla/5.0'}
 payload = {'code':'codehere','dob':'dobhere'}
 session = requests.Session()
-session.post(login_url, headers=headers, data=payload)
+resp = session.post(login_url, headers=headers, data=payload)
+print(resp.status_code)
