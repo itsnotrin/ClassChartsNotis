ce4455ce6872ed9e0e65eeac9b87c6ccba2a2c48
itsnotrin <ryanwfun@gmail.com>
2021-09-29 18:50:49 +0100

    Polished the login system significantly

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import requests
 
 #Urls:
-# Homework URL: https://www.classcharts.com/apipublic/homework
-# Login URL: https://www.classcharts.com/apiv2student/login
-
-# Login System:
+homework_url = 'https://www.classcharts.com/apipublic/homework'
 login_url = 'https://www.classcharts.com/apiv2student/login'
-headers = {'User-Agent': 'Mozilla/5.0'}
-payload = {'code':'codehere','dob':'dobhere'}
-session = requests.Session()
-resp = session.post(login_url, headers=headers, data=payload)
-print(resp.status_code)
+
+# Login System: 
+def login(code, dob):
+	headers = {'User-Agent': 'Mozilla/5.0'}
+	payload = {'code':code,'dob':dob,'remember_me': '1'}
+	session = requests.Session()
+	resp = session.post(login_url, headers=headers, data=payload)
+	jsonResponse = resp.json()
+	if jsonResponse["success"] == 0:
+		print("Error while looging - Your date of birth or your login code is incorrect!")
+	else:
+		name = jsonResponse["data"]["name"]
+		print(f"Signed in as {name}!")
+
+
+login('', '')
